ae8503f6f217dba8b9353c37ce030f8af8fa3a3e
Aldous Cao <aycu9@k12albemarle.org>
2018-05-12 20:52:06 -0400

    Fix Gradle issues

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: build/install/chess-server/bin/chess-server
--- src/main/java/com/github/aycu9/Main.java
+++ src/main/java/com/github/aycu9/Main.java
@@ -6,6 +6,6 @@ package com.github.aycu9;
 public class Main {
 
     public static void main (String[]args){
-
+        System.out.println("hello :D");
     }
 }
